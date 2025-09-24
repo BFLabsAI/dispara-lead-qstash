f1d2babfcce0e49517717cd3ea2d4880090877ab
BFLabsAI <tecnologia@bflabs.com.br>
2025-09-24 20:17:20 -0300

    [dyad] Corrigindo erro de contexto do Sidebar - wrote 1 file(s)

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -1,15 +1,17 @@
 "use client";
 
 import { Outlet } from "react-router-dom";
-import { Sidebar } from "@/components/ui/sidebar"; // Assumindo que o sidebar está aqui
+import { Sidebar, SidebarProvider } from "@/components/ui/sidebar"; // Importando o SidebarProvider
 
 export function DashboardLayout() {
   return (
-    <div className="flex min-h-screen w-full">
-      <Sidebar />
-      <main className="flex-1 p-4 sm:p-6 lg:p-8 overflow-auto">
-        <Outlet />
-      </main>
-    </div>
+    <SidebarProvider>
+      <div className="flex min-h-screen w-full">
+        <Sidebar />
+        <main className="flex-1 p-4 sm:p-6 lg:p-8 overflow-auto">
+          <Outlet />
+        </main>
+      </div>
+    </SidebarProvider>
   );
 }
