f72b1d17312d9820af286b936a70875a8ee24983
BFLabsAI <tecnologia@bflabs.com.br>
2025-09-24 22:04:26 -0300

    [dyad] Cabeçalho fixo no topo da página - wrote 1 file(s)

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -7,11 +7,11 @@ import { Header } from "./Header";
 export function DashboardLayout() {
   return (
     <SidebarProvider>
-      <div className="flex min-h-screen w-full">
+      <div className="flex h-screen w-full overflow-hidden">
         <Sidebar />
         <div className="flex flex-1 flex-col">
           <Header />
-          <main className="flex-1 p-4 sm:p-6 lg:p-8 overflow-auto">
+          <main className="flex-1 p-4 sm:p-6 lg:p-8 overflow-y-auto">
             <Outlet />
           </main>
         </div>
